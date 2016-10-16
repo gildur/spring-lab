e7d3f50002b327b9daaf43296412b60fc7c267d7
Piotr Wolny <pwolny@e-point.pl>
2016-10-16 12:55:52 +0200

    Initial commit.

---
+++ src/main/java/pl/epoint/spring/lab/SpringLabApplication.java
@@ -0,0 +1,15 @@
+package pl.epoint.spring.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Piotr Wolny
+ */
+@SpringBootApplication
+public class SpringLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabApplication.class, args);
+    }
+}
